655340261272c187c40b82c2cb86e6cdad390bd4
zzjtnb <zhengzhujituan@gmail.com>
2022-04-04 17:00:19 +0800

    添加'vite-svg-loader

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 import Inspect from 'vite-plugin-inspect';
+import svgLoader from 'vite-svg-loader';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import {ElementPlusResolver} from 'unplugin-vue-components/resolvers';
@@ -18,6 +19,7 @@ export default defineConfig({
   plugins: [
     vue(),
     Inspect(),
+    svgLoader(),
     AutoImport({
       // /* options */
       imports: [

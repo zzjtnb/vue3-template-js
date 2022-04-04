023cab325e1d37999f175fd0d119cac147eeab9f
zzjtnb <zhengzhujituan@gmail.com>
2022-04-05 02:50:45 +0800

    打包错误

--- vite.config.js
+++ vite.config.js
@@ -23,10 +23,25 @@ export default defineConfig({
     },
   },
   css: {
+    // https://github.com/vitejs/vite/issues/5833
+    postcss: {
+      plugins: [
+        {
+          postcssPlugin: 'internal:charset-removal',
+          AtRule: {
+            charset: (atRule) => {
+              if (atRule.name === 'charset') {
+                atRule.remove();
+              }
+            },
+          },
+        },
+      ],
+    },
     preprocessorOptions: {
       scss: {
         additionalData: `@use "~/assets/styles/variable/index.scss" as *;`,
-        charset: false,
+        // charset: false,//不起作用
       },
     },
   },
@@ -60,13 +75,13 @@ export default defineConfig({
           '@vueuse/core': ['useEventListener', 'tryOnUnmounted', 'useTimeoutFn', 'useDraggable', 'useResizeObserver', 'useWindowSize'],
         },
       ],
-      // // 可以选择auto-import.d.ts生成的位置，使用ts建议设置为'src/auto-import.d.ts'
+      // // 可以选择auto-import.d.ts生成的位置,使用ts建议设置为'src/auto-import.d.ts'
       // // dts: 'types/auto-import.d.ts',
       dts: false,
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      //用于搜索组件的目录的相对路径。
+      //用于搜索组件的目录的相对路径.
       dirs: ['src/components/auto', 'src/assets/svg'],
       //组件的有效文件扩展名
       extensions: ['vue', 'svg', 'js'],
@@ -92,17 +107,17 @@ export default defineConfig({
     }),
   ],
   build: {
-    // 如果设置为false，整个项目中的所有 CSS 将被提取到一个 CSS 文件中
+    // 如果设置为false,整个项目中的所有 CSS 将被提取到一个 CSS 文件中
     cssCodeSplit: true,
-    // 构建后是否生成 source map 文件。如果为 true，将会创建一个独立的 source map 文件
+    // 构建后是否生成 source map 文件.如果为 true,将会创建一个独立的 source map 文件
     sourcemap: false,
-    // 设置最终构建的浏览器兼容目标。默认值是一个 Vite 特有的值——'modules'  还可设置为 'es2015' 'es2016'等
+    // 设置最终构建的浏览器兼容目标.默认值是一个 Vite 特有的值--'modules'  还可设置为 'es2015' 'es2016'等
     target: 'modules',
     // 单位kb  打包后文件大小警告的限制 (文件大于此此值会出现警告)
     chunkSizeWarningLimit: 1000,
-    // 单位字节（1024等于1kb） 小于此阈值的导入或引用资源将内联为 base64 编码，以避免额外的 http 请求。设置为 0 可以完全禁用此项。
+    // 单位字节(1024等于1kb) 小于此阈值的导入或引用资源将内联为 base64 编码,以避免额外的 http 请求.设置为 0 可以完全禁用此项.
     assetsInlineLimit: 4096,
-    // 'terser' 相对较慢，但大多数情况下构建后的文件体积更小。'esbuild' 最小化混淆更快但构建后的文件相对更大。
+    // 'terser' 相对较慢,但大多数情况下构建后的文件体积更小.'esbuild' 最小化混淆更快但构建后的文件相对更大.
     minify: 'terser',
     terserOptions: {
       compress: {
